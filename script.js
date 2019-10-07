f3dff6d46f00398652a941a7bc1da117c53a7ee4
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-10-07 10:51:19 +0800

    changed link to jquery

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-//AJAX CALL TO CHUCK NORRIS JOKE RANDOMIZER
-
-const jokeButton = document.getElementById("joke-button");
-
-let jokeArea = document.getElementById("joke-container");
-
-jokeButton.addEventListener("click", function() {
-  $.ajax({
-    url: "https://api.chucknorris.io/jokes/random",
-    method: "GET",
-    success: function(result) {
-      console.log(result);
-      console.log(result.value);
-      jokeArea.innerHTML = "";
-      jokeArea.innerHTML = result.value;
-    },
-    error: function(error) {
-      console.log(`Error: ${error}`);
-    }
-  });
-});
-
 //AJAX CALL TO GIPHY
 
 const gifButton = document.getElementById("image-button");
@@ -28,6 +6,16 @@ let gifArea = document.getElementById("image-container");
 //CREATE IMAGE IN JAVASCRIPT
 
 gifButton.addEventListener("click", function() {
+  // fetch("https://api.chucknorris.io/jokes/random")
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(data) {
+  //     console.log(data);
+  //     console.log(data.value);
+  //     jokeArea.innerHTML = "";
+  //     jokeArea.innerHTML = data.value;
+  //   });
   $.ajax({
     url:
       "https://api.giphy.com/v1/gifs/random?api_key=K93XvEF6TLGx2ohemi8JkMJshHM2nZHp&tag=&rating=G",
@@ -46,16 +34,39 @@ gifButton.addEventListener("click", function() {
   });
 });
 
-// fetch("https://api.chucknorris.io/jokes/random")
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     console.log(data);
-//     console.log(data.value);
-//     jokeArea.innerHTML = "";
-//     jokeArea.innerHTML = data.value;
-//   });
+//AJAX CALL TO CHUCK NORRIS JOKE RANDOMIZER
+
+const jokeButton = document.getElementById("joke-button");
+
+let jokeArea = document.getElementById("joke-container");
+
+jokeButton.addEventListener("click", function() {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then(function(response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      console.log(data.value);
+      jokeArea.innerHTML = "";
+      jokeArea.innerHTML = data.value;
+    });
+  console.log("I'm after the fetch!!!");
+  // $.ajax({
+  //   url: "https://api.chucknorris.io/jokes/random",
+  //   method: "GET",
+  //   success: function(result) {
+  //     console.log(result);
+  //     console.log(result.value);
+  //     jokeArea.innerHTML = "";
+  //     jokeArea.innerHTML = result.value;
+  //   },
+  //   error: function(error) {
+  //     console.log(`Error: ${error}`);
+  //   }
+  // });
+});
 
 //fetch(
 //   "https://api.giphy.com/v1/gifs/random?api_key=K93XvEF6TLGx2ohemi8JkMJshHM2nZHp&tag=&rating=G"
